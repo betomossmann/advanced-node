c500d0b1794aefc7fe5a918c4b1386b38bb72dca
Beto Mossmann <gilbertomossmann@gmail.com>
2023-05-13 15:55:43 -0300

    move SUT creation strategy to use beforeEach

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,70 +3,24 @@ import { FacebookAuthenticationService } from '@/data/services'
 import { AuthError } from '@/domain/errors'
 import { mock, type MockProxy } from 'jest-mock-extended'
 
-class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-  token?: string
-  callsCount = 0
-  result: undefined
-
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    this.token = params.token
-    this.callsCount++
-    return this.result
-  }
-}
-
-type SutTypes = {
-  sut: FacebookAuthenticationService
-  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
-}
-
-const makeSut = (): SutTypes => {
-  const loadFacebookUserApi = mock<LoadFacebookUserApi>()
-  const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-  return {
-    sut,
-    loadFacebookUserApi
-  }
-}
-
 describe('FacebookAuthenticationService', () => {
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let sut: FacebookAuthenticationService
+
+  beforeEach(() => {
+    loadFacebookUserApi = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
+
   it('Should call LoadFacebookUserApi with correct params', async () => {
-    const { sut, loadFacebookUserApi } = makeSut()
-
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
-  // Another example(Same test as above)
-  it('Should call LoadFacebookUserApi with correct params(demo with jest)', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-
-    await sut.perform({ token: 'any_token' })
-
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
-  })
-
-  it('Should return AuthError when LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
-    loadFacebookUserApi.result = undefined
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-
-    const authResult = await sut.perform({ token: 'any_token' })
-
-    expect(authResult).toEqual(new AuthError())
-  })
-
-  // Another example(Same test as above)
-  it('Should return AuthError when LoadFacebookUserApi returns undefined(demo with mock-jest)', async () => {
-    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
+  it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
 
---
+++ tests/examples/test-facebook-api.ts
@@ -0,0 +1,86 @@
+import { type LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookAuthenticationService } from '@/data/services'
+import { AuthError } from '@/domain/errors'
+import { mock, type MockProxy } from 'jest-mock-extended'
+
+class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
+  token?: string
+  callsCount = 0
+  result: undefined
+
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
+    this.token = params.token
+    this.callsCount++
+    return this.result
+  }
+}
+
+type SutTypes = {
+  sut: FacebookAuthenticationService
+  loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+}
+
+const makeSut = (): SutTypes => {
+  const loadFacebookUserApi = mock<LoadFacebookUserApi>()
+  const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  return {
+    sut,
+    loadFacebookUserApi
+  }
+}
+
+describe('FacebookAuthenticationService', () => {
+  it('Should call LoadFacebookUserApi with correct params', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    await sut.perform({ token: 'any_token' })
+
+    expect(loadFacebookUserApi.token).toBe('any_token')
+    expect(loadFacebookUserApi.callsCount).toBe(1)
+  })
+
+  // Another example(Same test as above)
+  it('Should call LoadFacebookUserApi with correct params(demo with jest)', async () => {
+    const loadFacebookUserApi = {
+      loadUser: jest.fn()
+    }
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    await sut.perform({ token: 'any_token' })
+
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
+  })
+
+  // Another example(Same test as above)
+  it('Should call LoadFacebookUserApi with correct params', async () => {
+    const { sut, loadFacebookUserApi } = makeSut()
+
+    await sut.perform({ token: 'any_token' })
+
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return AuthError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthError())
+  })
+
+  // Another example(Same test as above)
+  it('Should return AuthError when LoadFacebookUserApi returns undefined(demo with mock-jest)', async () => {
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthError())
+  })
+})
