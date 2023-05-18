78e51f15b2a3f3ab9631eb95268955999a511e09
Beto Mossmann <gilbertomossmann@gmail.com>
2023-05-18 14:18:47 -0300

    share SUT creation

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -5,26 +5,26 @@ import { type Repository, getRepository, getConnection } from 'typeorm'
 import { type IBackup } from 'pg-mem'
 
 describe('PostgresUserAccountRepository', () => {
+  let sut: PostgresUserAccountRepository
+  let pgUserRepo: Repository<PgUser>
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser])
+    backup = db.backup()
+    pgUserRepo = getRepository(PgUser)
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PostgresUserAccountRepository()
+  })
+
   describe('load', () => {
-    let sut: PostgresUserAccountRepository
-    let pgUserRepo: Repository<PgUser>
-    let backup: IBackup
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([PgUser])
-      backup = db.backup()
-      pgUserRepo = getRepository(PgUser)
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
-    beforeEach(() => {
-      backup.restore()
-      sut = new PostgresUserAccountRepository()
-    })
-
     it('Should return an account if email exists', async () => {
       await pgUserRepo.save({ email: 'existing_email' })
 
