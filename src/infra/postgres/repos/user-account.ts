4851c61e33754c5aa9833319c3e184bf4c60e27b
Beto Mossmann <gilbertomossmann@gmail.com>
2023-05-18 15:02:22 -0300

    improve code quality

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -2,10 +2,22 @@ import { type SaveFacebookAccountRepository, type LoadUserAccountRepository } fr
 import { PgUser } from '@/infra/postgres/entities'
 import { getRepository } from 'typeorm'
 
+/**
+ * You can use type alias to shorten the name, example:
+ *
+ * type LoadParams = LoadUserAccountRepository.Params
+ * type LoadResult = LoadUserAccountRepository.Result
+ *
+ * type SaveParams = SaveFacebookAccountRepository.Params
+ * type SaveResult = SaveFacebookAccountRepository.Result
+ *
+ */
+
 export class PostgresUserAccountRepository implements LoadUserAccountRepository {
+  private readonly pgUserRepo = getRepository(PgUser)
+
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    const pgUser = await this.pgUserRepo.findOne({ email: params.email })
     if (pgUser !== undefined) {
       return {
         id: pgUser?.id.toString(),
@@ -15,15 +27,14 @@ export class PostgresUserAccountRepository implements LoadUserAccountRepository 
   }
 
   async saveWithFacebook (params: SaveFacebookAccountRepository.Params): Promise<void> {
-    const pgUserRepo = getRepository(PgUser)
     if (params.id === undefined) {
-      await pgUserRepo.save({
+      await this.pgUserRepo.save({
         email: params.email,
         name: params.name,
         facebookId: params.facebookId
       })
     } else {
-      await pgUserRepo.update({
+      await this.pgUserRepo.update({
         id: parseInt(params.id)
       }, {
         name: params.name,
