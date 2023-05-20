fbd81614377a2113fbfa1e09f13b54cbf22297c0
Beto Mossmann <gilbertomossmann@gmail.com>
2023-05-20 09:40:59 -0300

    ensure FacebookApi returns undefined when HttpGetClient throws

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('Should', () => {
-    expect(1).toBe(1)
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('Facebook API integration tests', () => {
+  it('Should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: '' })
+
+    expect(fbUser).toEqual({
+      facebookId: '',
+      email: '',
+      name: ''
+    })
+  })
+
+  it('Should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
