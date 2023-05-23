6a4f6fb8427828ce6e9ea784226e22f9169756ab
Beto Mossmann <gilbertomossmann@gmail.com>
2023-05-23 12:07:56 -0300

    rename JwtTokenGenerrator to JwtTokenHandler

--- src/main/factories/usecases/facebook-authentication.ts
+++ src/main/factories/usecases/facebook-authentication.ts
@@ -1,12 +1,12 @@
 import { setupFacebookAuthentication, type FacebookAuthentication } from '@/domain/usecases'
 import { makePgUserAccountRepo } from '@/main/factories/repos'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 import { makeFacebookApi } from '@/main/factories/apis'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
